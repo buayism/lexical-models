d46c2191fcb0d4391fec2f35be08518bbb6dd163
Buay Kun Gach <buaykungach028@gmail.com>
2026-09-22 09:34:27 +0300

    edit some words

--- release/b/buay_kun_gach_deng.nus.nuer/source/buay_kun_gach_deng.nus.nuer.model.ts
+++ release/b/buay_kun_gach_deng.nus.nuer/source/buay_kun_gach_deng.nus.nuer.model.ts
@@ -1,9 +1,30 @@
+/// <reference types="@keymanapp/lexical-model-compiler/dist/kmlmc.d.ts" />
+
 const source: LexicalModelSource = {
   format: 'trie-1.0',
-  wordBreaker: {
-    use: 'default',
+
+  sources: [
+    'book_wordlist.tsv',
+    'natural_sentences_wordlist.tsv',
+    'bible_wordlist.tsv',
+  ],
+
+  wordBreaker: function (text: string): Span[] {
+    const wordPattern = /\/[\p{L}\p{M}]+|[\p{L}\p{M}]+/gu;
+    const words: Span[] = [];
+    let match: RegExpExecArray | null;
+
+    while ((match = wordPattern.exec(text)) !== null) {
+      words.push({
+        text: match[0],
+        start: match.index,
+        end: match.index + match[0].length,
+        length: match[0].length,
+      });
+    }
+
+    return words;
   },
-  /* Point Keyman to both of your custom Nuer lists */
-  sources: ['book_wordlist.tsv', 'natural_sentences_wordlist.tsv', 'bible_wordlist.tsv'],
 };
+
 export default source;
